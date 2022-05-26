a01b0141ecd1593194345db3cdc9491f536aeedd
RhysWood <rhyswoody@gmail.com>
2022-05-25 22:12:01 -0700

    initial commit

---
+++ src/components/welcome.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/welcome.css';
+
+function Welcome() {
+    return ( 
+
+          <div class="wrapper">
+              <div class="typing-demo">
+                This is a portfolio by Rhys Wood.
+              </div>
+          </div>
+     );
+}
+
+export default Welcome;
