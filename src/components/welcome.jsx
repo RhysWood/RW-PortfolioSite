ecb52471d406dfc0f6f6e971ac3c478e8522f7e5
RhysWood <rhyswoody@gmail.com>
2022-06-01 16:47:17 -0700

    add smooth scroll & favicon

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -3,12 +3,36 @@ import '../styles/welcome.css';
 
 function Welcome() {
     return ( 
+      <>
 
-          <div class="wrapper">
+      <div class="wrapperparent">
+        <div class="section">
+        <div class="wrapper">
               <div class="typing-demo">
                 This is a portfolio by Rhys Wood.
               </div>
           </div>
+        </div>
+        
+        <div class="section bg-blue">
+          Content 2
+        </div>
+        
+        <div class="section bg-green">
+          Content 3
+        </div>
+        
+        <div class="section">
+          Content 4
+        </div>
+        
+        <div class="section bg-blue">
+          Content 5
+        </div>
+      </div>
+
+        
+      </>
      );
 }
 
