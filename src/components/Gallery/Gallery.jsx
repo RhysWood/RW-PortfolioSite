69dc07757da202b76b682681f271650f37f0e802
Rhys <rhyswoody@gmail.com>
2023-01-27 14:23:07 -0800

    test img Tim

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -8,11 +8,10 @@ import "./style.scss";
 
 const images = [
   {
-    src:
-      "https://images.unsplash.com/photo-1566204773863-cf63e6d4ab88?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1345&q=100",
-    title: "Dracaena Trifasciata",
-    subtitle: "Live the Beauty",
-    category: "Shooting / Adv.Campaing",
+    src:"../Imgs/Tim.png",
+    title: "FRONT-END DEVELOPMENT",
+    subtitle: "Fully Craft-CMS Integrated",
+    category: "meetgoat.com", 
   },
   {
     src:
@@ -69,8 +68,8 @@ function GalleryItem({
         </div>
         <div
           className="gallery-item-image"
-          style={{ backgroundImage: `url(${src})` }}
-        ></div>
+          style={{ backgroundImage: `url(${src})` }}>
+        </div>
       </div>
       <div></div>
     </div>
