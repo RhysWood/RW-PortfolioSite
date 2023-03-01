b291468182bb997fa3ce36df2feafd5fd465b9a0
Rhys <rhyswoody@gmail.com>
2023-02-28 17:47:27 -0800

    test on gallery update

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,5 +1,5 @@
-import React, {useState, useRef, useEffect} from "react";
-import '../styles/index.scss';
+import React, { useState, useRef, useEffect } from "react";
+import "../styles/index.scss";
 import Lighthouse from "./Lighthouse/Lighthouse";
 import IntroText from "./Welcome/IntroText";
 // import Goat from "./Goat/Goat";
@@ -12,22 +12,19 @@ import NavBar from "./Navigation/navbar";
 import Footer from "./Navigation/Footer";
 import useLocoScroll from "./hooks/useLocoScroll";
 
-
 const Home = () => {
-
   const [isVisible, setIsVisible] = useState(false);
   const [preloader, setPreloader] = useState(true);
   useLocoScroll(!preloader);
   const [timer, setTimer] = useState(2);
 
-  
   const timeoutId = useRef(null);
-  
+
   const clear = () => {
     clearTimeout(timeoutId.current);
     setPreloader(false);
   };
-  
+
   useEffect(() => {
     if (timer > 0) {
       timeoutId.current = setTimeout(() => {
@@ -38,7 +35,6 @@ const Home = () => {
     }
   }, [timer]);
 
-
   useEffect(() => {
     if (!preloader) {
       setTimeout(() => {
@@ -47,54 +43,56 @@ const Home = () => {
     }
   }, [preloader]);
 
-
-    return ( 
-      <>
+  return (
+    <>
       <CustomCursor />
-      {preloader? (
+      {preloader ? (
         <div className="loader-wrapper absolute" ref={timeoutId}>
           <h1>RW</h1>
-        </div> )
-      : (
-      <>
-      <div className="main-container" id="main-container" data-scroll-container>
-        <div className={`fade-in-all ${isVisible ? "show" : ""}`}>
-        <NavBar />
-          <div className="section">
-            <Welcome />
-          </div>
+        </div>
+      ) : (
+        <>
+          <div
+            className="main-container"
+            id="main-container"
+            data-scroll-container
+          >
+            <div className={`fade-in-all ${isVisible ? "show" : ""}`}>
+              <NavBar />
+              <div className="section">
+                <Welcome />
+              </div>
 
-          <div className="section">
-            <IntroText />
-          </div>
+              <div className="section">
+                <IntroText />
+              </div>
 
-          <div className="section">
-            <Lighthouse />
-          </div>
+              <div className="section">
+                <Lighthouse />
+              </div>
 
-          <div id="gallery">
-            <Gallery />
-          </div> 
+              <div id="gallery">
+                <Gallery />
+              </div>
 
-          {/* <div id="vessel" className="section">
+              {/* <div id="vessel" className="section">
             <Goat />
           </div> */}
-    
-          <div id="vessel" className="section">
-            <Vessel />
+
+              <div id="vessel" className="section">
+                <Vessel />
+              </div>
+
+              <div id="contact" className="section">
+                <Contact />
+              </div>
+            </div>
+            <Footer />
           </div>
-    
-          <div id="contact" className="section">
-            <Contact />
-          </div>
-        </div>
-        <Footer />
-        </div>
-      </>)
-      }
-      </>
-     );
-    
-}
+        </>
+      )}
+    </>
+  );
+};
 
 export default Home;
